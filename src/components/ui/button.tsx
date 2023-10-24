cb9d4e0f1f649939fc7c4c6ae0e1dadfa12e5aae
Hans Tanawi <hans.tanawi@outlook.com>
2023-10-24 22:48:15 +0700

    fix: button props

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -2,9 +2,9 @@ import { ButtonHTMLAttributes, type PropsWithChildren } from 'react';
 
 type ButtonProps = PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>;
 
-export default function Button({ children, onClick }: ButtonProps) {
+export default function Button({ children, ...props }: ButtonProps) {
   return (
-    <button className="bg-black text-white" onClick={onClick}>
+    <button className="bg-black text-white" {...props}>
       {children}
     </button>
   );
