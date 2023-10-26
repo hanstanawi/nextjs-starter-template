39c2742d27ba18a2c418658d832697f800d91639
Hans Tanawi <hans.tanawi@outlook.com>
2023-10-26 21:45:20 +0700

    feat: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link';
 import Image from 'next/image';
+import Button from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative z-[-1] flex place-items-center before:absolute before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+    <main className="flex h-screen flex-col items-center justify-between px-24 pb-24 pt-20">
+      <div className="relative flex place-items-center before:absolute before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <div className="flex h-full flex-col items-center justify-center">
           <Image
             className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -13,15 +15,18 @@ export default function Home() {
             height={37}
             priority
           />
-          <h1 className="mt-8 text-center text-4xl font-semibold">
+          <h1 className="mt-8 text-center text-4xl font-extrabold">
             Starter Template
           </h1>
-          <p className="mt-4 text-center text-base text-black opacity-60">
+          <p className="mt-4 text-center text-base opacity-60 dark:opacity-80">
             A starter template to kickstart your Next.js project
           </p>
-          <p></p>
-          <ul className="mt-8 text-center text-black opacity-50">
-            <li>Next.js 13 with app directory</li>
+          <p className="mt-8 text-start text-xl font-semibold">
+            What&apos; inside?
+          </p>
+          <ul className="mb-6 mt-4 list-disc text-black opacity-50 dark:text-white dark:opacity-80">
+            <li>Next.js 13 with app dir</li>
+            <li>React</li>
             <li>TailwindCSS</li>
             <li>TypeScript</li>
             <li>ESLint</li>
@@ -31,6 +36,15 @@ export default function Home() {
             <li>Dependabot</li>
             <li>Jest + React Testing Library</li>
           </ul>
+          <Button>
+            <Link
+              href="https://github.com/hanstanawi/nextjs-starter-template"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Learn more
+            </Link>
+          </Button>
         </div>
       </div>
     </main>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,10 +1,18 @@
+import { cn } from '@/lib/utils';
 import { ButtonHTMLAttributes, type PropsWithChildren } from 'react';
 
 type ButtonProps = PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>;
 
 export default function Button({ children, ...props }: ButtonProps) {
   return (
-    <button className="bg-black text-white" {...props}>
+    <button
+      className={cn(
+        `cursor-pointer rounded-lg border border-black  
+        bg-black px-4 py-3 text-white dark:border-white dark:bg-transparent 
+        dark:hover:bg-white dark:hover:bg-opacity-10 md:hover:bg-opacity-80 md:hover:text-white`,
+      )}
+      {...props}
+    >
       {children}
     </button>
   );
