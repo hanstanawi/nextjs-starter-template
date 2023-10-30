deb5d98cd03782534b4ff2ebe5d116c028f796eb
Hans Tanawi <hans.tanawi@outlook.com>
2023-10-30 22:08:12 +0700

    fix: add jest custom config

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,23 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/test/jest.setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
   preset: 'ts-jest',
+  // A list of paths to directories that Jest should use to search for files in
+  // https://jestjs.io/docs/configuration#roots-arraystring
+  roots: ['<rootDir>/src/'],
+  // Jest transformations
+  // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest', // Transform TypeScript files using ts-jest
+  },
+  // Code coverage config
+  // https://jestjs.io/docs/configuration#collectcoveragefrom-array
+  coverageDirectory: '<rootDir>/coverage/',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!**/__mocks__/**',
+    '!**/node_modules/**',
+    '!**/*.d.ts',
+  ],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
